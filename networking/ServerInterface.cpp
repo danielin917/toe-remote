6dc4c993d5d2ad6602d5f36c1afab30537683c75
danielin917 <danielin@umich.edu>
2016-02-20 19:53:31 -0500

    button struct

---
+++ networking/ServerInterface.cpp
@@ -0,0 +1,4 @@
+#include "ServerInterface.h"
+#include<iostream>
+
+
--- networking/ServerInterface.h
+++ networking/ServerInterface.h
@@ -1,17 +1,39 @@
 #pragma once
 
-/**
+#include<functional>
+#include<vector>
+#include<string>
+#include<unordered_map>
+
+
+/*
+ *Button settings specified by developer
+ */
+struct Button{
+	int id;
+	int size_x;
+	int size_y;
+	int grid_x;
+	int grid_y;
+	std::string text;
+	
+	Button(int _id, int _size_x, int _size_y, int _grid_x, int _grid_y, std::string _text)
+	:id(_id), size_x(_size_x), size_y(_size_y), grid_x(_grid_x), grid_y(_grid_y), text(_text){}
+};
+
+/*
  * Parameters required to start the server.
  */
 struct ServerParameters {
-  
+	std::vector<Button*> btn_vec;
+	std::unordered_map<int, std::function<void()> > function_map;		  
+	int create_button(int id, int size_x, int size_y, int grid_x, int grid_y, std::string text)
+	{
+		Button* btn = new Button(id, size_x, size_y, grid_x, grid_y, text);
+		btn_vec.push_back(btn);
+		return 1;	
+	}	
 };
-
-enum class Commands {
-  RequestName,
-
-};
-
 class ServerInterface {
 public:
   /**
