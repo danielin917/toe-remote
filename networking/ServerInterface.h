e13c520d5c85cf77236854ebcb1ec23ad50e8029
danielin917 <danielin@umich.edu>
2016-02-20 20:26:34 -0500

    no more parameter object

--- networking/ServerInterface.h
+++ networking/ServerInterface.h
@@ -21,25 +21,26 @@ struct Button{
 	:id(_id), size_x(_size_x), size_y(_size_y), grid_x(_grid_x), grid_y(_grid_y), text(_text){}
 };
 
-/*
- * Parameters required to start the server.
- */
-struct ServerParameters {
-	std::vector<Button*> btn_vec;
-	std::unordered_map<int, std::function<void()> > function_map;		  
-	int create_button(int id, int size_x, int size_y, int grid_x, int grid_y, std::string text)
-	{
-		Button* btn = new Button(id, size_x, size_y, grid_x, grid_y, text);
-		btn_vec.push_back(btn);
-		return 1;	
-	}	
-};
 class ServerInterface {
 public:
-  /**
+  
+	//STORAGE FOR BUTTONS
+	std::vector<Button*> btn_vec;
+	//MAPPINNG FROM ID TO FUNCTION
+	std::unordered_map<int, std::function<void()> > function_map;		  
+//////////////////////SETTINGS//////////////////////////////////
+/*
+ *Create and store new button
+ *RETURNS: 1 on success
+ */
+	int create_button(int id, int size_x, int size_y, int grid_x, int grid_y, std::string text, std::function<void()> func);		
+
+  
+//////////////////////INTERFACE/////////////////////////////////
+ /**
    * Constructs the object and starts the server
    */
-  ServerInterface(ServerParameters params);
+  ServerInterface(){}
   /**
    * Starts the server
    */
@@ -61,4 +62,4 @@ public:
 
   // Its going to be a bit harder to define how the communication works.
   // Probably do an example, then work it out
-};
+ };
