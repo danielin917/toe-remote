d837386962b225bd616dd284b63402ddb173c02b
Nick Terrell <terrelln@umich.edu>
2016-02-20 20:57:37 -0500

    Fix mistakenly removed files.

---
+++ app/toe-remote/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  toe-remote
+//
+//  Created by Nick Terrell on 2/20/16.
+//  Copyright © 2016 eecs481. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ app/toe-remote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  toe-remote
+//
+//  Created by Nick Terrell on 2/20/16.
+//  Copyright © 2016 eecs481. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
