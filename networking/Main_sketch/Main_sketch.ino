865d56bf1560f35cfc0cccb6e13adf59fbba2e56
danielin917 <danielin@umich.edu>
2016-02-21 17:54:37 -0500

    Compiled in Arduino, early architecture without stl

---
+++ networking/Main_sketch/Main_sketch.ino
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "ServerInterface.h"
+
+void setup(){
+
+}
+
+void loop(){
+
+
+}
---
+++ networking/Main_sketch/lib.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<Arduino.h>
+template<typename T>
+class Vector{
+	T* array;
+	//length of array
+	size_t length;
+	//data in array
+	size_t count;
+	
+	void grow()
+	{
+		T* creation = new T[length * 2];
+		memcpy(creation, array, length*sizeof(T));
+		delete [] array;
+		array = creation;
+		length *= 2;
+	}
+public:
+	Vector():array(new T[1]), length(1), count(0){}
+	
+	void push_back(T data)
+	{
+		if(count == length)
+			grow();
+		
+		array[count] = data;
+		count += 1;
+	}
+	void pop_back()
+	{
+		
+	}
+	size_t size()
+	{
+		return count;
+	}			
+};
