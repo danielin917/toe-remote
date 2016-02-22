65780a746b3518913f86def3f698ee1590f51649
danielin917 <danielin@umich.edu>
2016-02-22 12:59:14 -0500

    tested vector

--- networking/Main_sketch/lib.h
+++ networking/Main_sketch/lib.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<Arduino.h>
+//#include <stdlib.h>
+//#include <string.h>
 template<typename T>
 class Vector{
 	T* array;
@@ -29,7 +31,7 @@ public:
 	}
 	void pop_back()
 	{
-		
+		count -= 1;	
 	}
 	size_t size()
 	{
@@ -37,6 +39,12 @@ public:
 	}
 	T operator[](int x)
 	{
+		/*some sort of bounds check
+		if(x >= count)
+		{
+			throw;
+		}
+		*/
 		return array[x];
 	}	
 };
