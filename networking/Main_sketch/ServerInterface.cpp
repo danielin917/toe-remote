58f624228e39ca0d6b2725ac15244b33d6df23f8
danielin917 <danielin@umich.edu>
2016-02-22 12:39:24 -0500

    call to function map

--- networking/Main_sketch/ServerInterface.cpp
+++ networking/Main_sketch/ServerInterface.cpp
@@ -89,7 +89,7 @@ bool ServerInterface::process_command(){
 bool ServerInterface::call_function(unsigned char cmd)
 {
 	int index = (int)cmd;
-	array[index]();		
+	function_map[index]();		
 	return true;
 }  
 
