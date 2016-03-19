c7bcd6fa2546b35264a3770913377b2a59de1682
Nick Terrell <terrelln@umich.edu>
2016-03-19 15:37:14 -0400

    Add partially working swift app.

---
+++ app/toe-remote/ButtonLayout.swift
@@ -0,0 +1,63 @@
+//
+//  ButtonLayout.swift
+//  toe-remote
+//
+//  Created by Nick Terrell on 3/19/16.
+//  Copyright © 2016 eecs481. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Button: NSObject {
+    var x: CGFloat
+    var y: CGFloat
+    var width: CGFloat
+    var height: CGFloat
+    var title: String
+    
+    var _view: UIButton?
+    
+    init(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, title: String) {
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.title = title
+        super.init()
+    }
+    
+    init(data: NSData) {
+        assert(data.length >= 55)
+        x = 0
+        y = 0
+        width = 0
+        height = 0
+        title = ""
+        
+        super.init()
+    }
+    
+    func view() -> UIView {
+        guard _view == nil else { return _view! }
+        
+        _view = UIButton.init(frame: CGRectMake(x, y, width, height))
+        _view!.setTitle(title, forState: .Normal)
+        return _view!
+    }
+}
+
+class ButtonLayout: NSObject {
+    var buttons: Array<Button>
+    
+    override init() {
+        self.buttons = Array<Button>()
+        super.init()
+    }
+    
+    func addToView(view: UIView) {
+        for button in buttons {
+            view.addSubview(button.view())
+        }
+    }
+}
---
+++ app/toe-remote/DeviceViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DeviceViewController.swift
+//  toe-remote
+//
+//  Created by Nick Terrell on 3/16/16.
+//  Copyright © 2016 eecs481. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class DeviceViewController: UIViewController/*, BLEDelegate*/ {
+    var ble: BLE?
+    var peripheral: CBPeripheral?
+    var buttonLayout: ButtonLayout?
+    
+    init(ble: BLE, peripheral: CBPeripheral, buttonLayout: ButtonLayout?) {
+        self.ble = ble
+        self.peripheral = peripheral
+        self.buttonLayout = buttonLayout
+        super.init(nibName: nil, bundle: nil)
+        //self.ble!.delegate = self
+    }
+    
+    override func viewDidLoad() {
+        buttonLayout?.addToView(self.view)
+        
+        guard let peripheral = peripheral else { return }
+        ble?.connectToPeripheral(peripheral)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    /*
+    func bleDidScanTimeout() { }
+    
+    func bleDidUpdateState(state: CBCentralManagerState) {
+        if state == .PoweredOn {
+        }
+    }
+    
+    func bleDidConnectToPeripheral() {
+        print("[DEBUG] Connected to peripheral")
+    }
+    
+    func bleDidDisconenctFromPeripheral() {
+        print("[DEBUG] Disconnected from peripheral")
+    }
+    
+    func bleDidReceiveData(data: NSData?) {
+        if let d = data {
+            NSLog("%@", d)
+        }
+    }
+    */
+}
