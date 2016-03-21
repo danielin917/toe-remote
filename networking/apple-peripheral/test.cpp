b4cc3e424bc0123f4fc7dde6e625fee37461395b
Nick Terrell <terrelln@umich.edu>
2016-03-20 22:12:48 -0400

    Add test mac app.

--- networking/apple-peripheral/test.cpp
+++ networking/apple-peripheral/test.cpp
@@ -1,23 +1,31 @@
+#include "../Main_sketch/ServerInterface.h"
 #include "BLEPeripheral.h"
 
-#include <unistd.h>
+#include <vector>
 #include <iostream>
 
+void a() { std::cout << "Button A pressed" << std::endl; }
+void b() { std::cout << "Button B pressed" << std::endl; }
+void c() { std::cout << "Button C pressed" << std::endl; }
+void r() { std::cout << "Button Rest pressed" << std::endl; }
+
 int main() {
-    BLEPeripheral p("hello");
-    unsigned count = 0;
-    for (;;) {
-        if (p.connected()) {
-            std::cout << "connected" << std::endl;
-            const char *hello = "hello";
-            p.write((const unsigned char*)hello, 6);
-            while (p.bytes_available() > 0) {
-                std::cout << '0' + p.read_byte() << std::endl;
-            }
-        } else {
-            std::cout << "not connected" << std::endl;
-        }
-        ++count;
-        sleep(1);
+    const char *name = "test-app";
+    const std::vector<Button> buttons = {
+        {0, 0, 30, 30, "Button A"},
+        {33, 0, 30, 30, "Button B"},
+        {66, 0, 30, 30, "Button C"},
+        {0, 33, 100, 67, "The Rest of the Screen"}};
+    const std::vector<button_func> funcs = {&a, &b, &c, &r};
+    ServerInterface server;
+    server.set_device_name(name);
+    for (unsigned i = 0; i < buttons.size(); ++i) {
+        const auto &button = buttons[i];
+        server.create_button(button.x, button.y, button.width, button.height,
+                             button.text, funcs[i]);
+    }
+    server.start_server();
+    while (true) {
+        server.process_command();
     }
 }
