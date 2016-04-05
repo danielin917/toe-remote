6d0b0adc2777e0f6116c19700075854e8b25ff6c
Nick Terrell <terrelln@umich.edu>
2016-04-05 01:02:28 -0400

    Start working on OS X GUI app.

---
+++ controller-maker/controller-maker/KeyboardAction.m
@@ -0,0 +1,19 @@
+//
+//  KeyboardAction.m
+//  controller-maker
+//
+//  Created by Nick Terrell on 4/3/16.
+//  Copyright © 2016 eecs481. All rights reserved.
+//
+
+#import "KeyboardAction.h"
+
+@implementation KeyboardAction
+-(instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
